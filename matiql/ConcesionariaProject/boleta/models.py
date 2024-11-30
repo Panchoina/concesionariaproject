05685269e08d4b8b8c222fd858bc25b9fdaa9d89
el-tuetue <matiascabrero18@gmail.com>
2024-11-29 23:10:08 -0300

    agrege models

--- matiql/ConcesionariaProject/boleta/models.py
+++ matiql/ConcesionariaProject/boleta/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class boleta(models.Model):
+    folio = models.IntegerField(primary_key=True,max_length=10)
+    monto = models.IntegerField(max_length=15)
+    direccion = models.CharField(max_length=50)
+    fono = models.IntegerField(max_length=9)
+    fonocliente = models.IntegerField()
+    vendedor = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
--- matiql/ConcesionariaProject/cliente/models.py
+++ matiql/ConcesionariaProject/cliente/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class cliente(models.Model):
+    rut = models.IntegerField(primary_key=True, max_length=9)
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField( max_length=50)
+    fono = models.IntegerField(max_length=9)
+    gmail = models.CharField(max_length=50)
+    contraseña = models.CharField( max_length=50)
+
+    def __str__(self):
+        return f"{self.nombre}"
+    class Meta:
+        db_table = "Cliente"
